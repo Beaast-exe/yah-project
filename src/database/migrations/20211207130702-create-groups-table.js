b0a56b3287b746cbe13e18bce651239ca4560ece
Beaast-exe <r.fariaa18@gmail.com>
2021-12-07 13:12:30 +0000

    implemented sequelize

---
+++ src/database/migrations/20211207130702-create-groups-table.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('groups', {
+			group_id: {
+				type: Sequelize.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+				allowNull: false
+			},
+			group_name: {
+				type: Sequelize.STRING,
+				allowNull: false
+			}
+		});
+	},
+
+
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('groups');
+	}
+
+};
